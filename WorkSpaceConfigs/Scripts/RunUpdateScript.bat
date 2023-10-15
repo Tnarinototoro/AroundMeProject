a365d74ecc93b47a46dbd08c6b07e2c963896297
Yorda <921864047@qq.com>
2023-10-16 00:04:11 +0900

    2333

--- WorkSpaceConfigs/Scripts/RunUpdateScript.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set "PS_SCRIPT_PATTH_N=D:\GitProjects\AroundMeProject\WorkSpaceConfigs\Scripts\startNgrok.ps1"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PS_SCRIPT_PATTH_N%""' -Verb RunAs}"
-
-
-set "PS_SCRIPT_PATH=D:\GitProjects\AroundMeProject\WorkSpaceConfigs\Scripts\UpdateGithubWebHookScript.ps1"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& {Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File ""%PS_SCRIPT_PATH%""' -Verb RunAs}"
--- WorkSpaceConfigs/Scripts/startNgrok.ps1
+++
@@ -1,5 +0,0 @@
-# 定义 ngrok 的完整路径和参数
-$ngrokCommand = "D:\GitProjects\AroundMeProject\WorkSpaceConfigs\Tools\ngrok-v3-stable-windows-amd64\ngrok.exe http 10086"
-
-# 启动 ngrok
-Invoke-Expression $ngrokCommand
