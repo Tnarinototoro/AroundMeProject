1c6c804416ac0a92c5f13a69cef3cb5aea35620b
Tnarinototoro <921864047@qq.com>
2024-09-04 09:51:47 +0900

    PG/Cam 自由度调整

--- AroundMe/Source/AroundMe/Player/Camera/DIY_MainPlayerCameraController.cpp
+++ AroundMe/Source/AroundMe/Player/Camera/DIY_MainPlayerCameraController.cpp
@@ -4,78 +4,67 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 
-
 UDIY_MainPlayerCameraController::UDIY_MainPlayerCameraController()
 {
-      // 启用每帧调用Tick()
+    // 启用每帧调用Tick()
     PrimaryComponentTick.bCanEverTick = true;
 
+    CurrentLookSpeed = FVector2D::ZeroVector;
+    TargetLookSpeed = FVector2D::ZeroVector;
+    LookAcceleration = FVector2D(2000.f, 2000.f);
+    LookSpeedInterpRate = 15.f;
 
-	CurrentLookSpeed = FVector2D::ZeroVector;
-	TargetLookSpeed = FVector2D::ZeroVector;
-	LookAcceleration = FVector2D(2000.f, 2000.f); 
-	LookSpeedInterpRate = 15.f;
+    // Create CameraBoom (Spring Arm component)
+    CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 
-	// Create CameraBoom (Spring Arm component)
-	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
-	
-	CameraBoom->SetupAttachment(this);
-	CameraBoom->TargetArmLength = 1300.f; 
-	CameraBoom->bUsePawnControlRotation = true; // Rotate arm based on controller
+    CameraBoom->SetupAttachment(this);
+    CameraBoom->TargetArmLength = 1300.f;
+    CameraBoom->bUsePawnControlRotation = true; // Rotate arm based on controller
 
+    // Create FollowCamera
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("TPSCamera"));
+    FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach camera to end of boom
+    FollowCamera->bUsePawnControlRotation = false;                              // Let the arm control the rotation
+                                                                                // if (CameraBoom->GetAttachParent() == GetCapsuleComponent())
+                                                                                // {
+                                                                                // 	UE_LOG(LogTemp, Warning, TEXT("CameraBoom is correctly attached to PlayerCapsule."));
+                                                                                // }
+                                                                                // else
+                                                                                // {
 
-	// Create FollowCamera
-	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("TPSCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);  // Attach camera to end of boom
-	FollowCamera->bUsePawnControlRotation = false;  // Let the arm control the rotation
-	// if (CameraBoom->GetAttachParent() == GetCapsuleComponent())
-	// {
-	// 	UE_LOG(LogTemp, Warning, TEXT("CameraBoom is correctly attached to PlayerCapsule."));
-	// }
-	// else
-	// {
-
-	// 	UE_LOG(LogTemp, Warning, TEXT("CameraBoom is NOT attached to PlayerCapsule."));
-	// }
-
-   
+    // 	UE_LOG(LogTemp, Warning, TEXT("CameraBoom is NOT attached to PlayerCapsule."));
+    // }
 }
 
 void UDIY_MainPlayerCameraController::BeginPlay()
 {
     Super::BeginPlay();
-	AController * cur_controller=AcquireOwnerPawnController();
+    AController *cur_controller = AcquireOwnerPawnController();
 
-	if(cur_controller)
-	{
-		FRotator NewRotator = cur_controller->GetControlRotation();
+    if (cur_controller)
+    {
+        FRotator NewRotator = cur_controller->GetControlRotation();
 
-		NewRotator.Pitch =340.f ;
+        NewRotator.Pitch = 340.f;
 
-	
-		
-	
-		cur_controller->SetControlRotation(NewRotator);
-	}
-	
+        cur_controller->SetControlRotation(NewRotator);
+    }
 }
 
 void UDIY_MainPlayerCameraController::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction *ThisTickFunction)
 {
     Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-
     UpdateUpDownCam(DeltaTime);
 }
 
-
 AController *UDIY_MainPlayerCameraController::AcquireOwnerPawnController()
 {
-	AActor* Owner = GetOwner();
+    AActor *Owner = GetOwner();
     if (Owner)
     {
-        
-        APawn* PawnOwner = Cast<APawn>(Owner);
+
+        APawn *PawnOwner = Cast<APawn>(Owner);
         if (PawnOwner)
         {
             // 获取并返回Pawn的Controller
@@ -86,105 +75,96 @@ AController *UDIY_MainPlayerCameraController::AcquireOwnerPawnController()
 }
 void UDIY_MainPlayerCameraController::UpdateUpDownCam(float DeltaTime)
 {
+#if 1
+    AController *cur_controller = AcquireOwnerPawnController();
 
-	AController * cur_controller=AcquireOwnerPawnController();
+    if (cur_controller)
+    {
+        float current_pitch_depot = UpDownCamPitchDepo[CurrentUpDownType];
+        bool should_execute_lerp{false};
 
-	if(cur_controller)
-	{
-float current_pitch_depot = UpDownCamPitchDepo[CurrentUpDownType];
-	bool should_execute_lerp{ false };
+        float target_pitch{0};
+        if (TargetUpDownType != CurrentUpDownType)
+        {
+            should_execute_lerp = true;
+            target_pitch = UpDownCamPitchDepo[(int)TargetUpDownType];
+        }
+        else
+        {
 
-	float target_pitch{ 0 };
-	if (TargetUpDownType != CurrentUpDownType)
-	{
-		should_execute_lerp = true;
-		target_pitch = UpDownCamPitchDepo[(int)TargetUpDownType];
-	}
-	else
-	{
+            if (cur_controller->GetControlRotation().Pitch != current_pitch_depot)
+            {
 
-		if (cur_controller->GetControlRotation().Pitch != current_pitch_depot)
-		{
+                target_pitch = current_pitch_depot;
+                should_execute_lerp = true;
+            }
+        }
 
-			target_pitch = current_pitch_depot;
-			should_execute_lerp = true;
-		}
+        if (should_execute_lerp)
+        {
 
-	}
+            FRotator NewRotator = cur_controller->GetControlRotation();
 
+            NewRotator.Pitch = FMath::FInterpTo(NewRotator.Pitch, target_pitch, DeltaTime, UpDownCameraLerpSpeed);
 
-	if (should_execute_lerp)
-	{
-		 
-		
-		FRotator NewRotator = cur_controller->GetControlRotation();
+            cur_controller->SetControlRotation(NewRotator);
 
-		NewRotator.Pitch = FMath::FInterpTo(NewRotator.Pitch, target_pitch, DeltaTime, UpDownCameraLerpSpeed);
+            float new_fov = FMath::FInterpTo(FollowCamera->FieldOfView, UpDownCamFOVDepo[TargetUpDownType], DeltaTime, UpDownCameraLerpSpeed);
 
-	
-		
-	
-		cur_controller->SetControlRotation(NewRotator);
+            FollowCamera->SetFieldOfView(new_fov);
 
+            float new_target_spring_length = FMath::FInterpTo(CameraBoom->TargetArmLength, UpDownCamSpringLengthDepo[TargetUpDownType], DeltaTime, UpDownCameraLerpSpeed);
 
-		float new_fov=FMath::FInterpTo(FollowCamera->FieldOfView,UpDownCamFOVDepo[TargetUpDownType],DeltaTime,UpDownCameraLerpSpeed);
+            CameraBoom->TargetArmLength = new_target_spring_length;
 
-
-		FollowCamera->SetFieldOfView(new_fov);
-
-
-		float new_target_spring_length=FMath::FInterpTo(CameraBoom->TargetArmLength,UpDownCamSpringLengthDepo[TargetUpDownType],DeltaTime,UpDownCameraLerpSpeed);
-
-
-		CameraBoom->TargetArmLength=new_target_spring_length;
-
-		if (FMath::Abs(cur_controller->GetControlRotation().Pitch - target_pitch) <= 0.1f)
-		{
-			CurrentUpDownType = TargetUpDownType;
-			
-		}
-		
-	}
-	//UE_LOG(MainPlayerLog, Warning, TEXT("xxxxxx cur rotation pitch %f"), GetControlRotation().Pitch);
-	}
-	
+            if (FMath::Abs(cur_controller->GetControlRotation().Pitch - target_pitch) <= 0.1f)
+            {
+                CurrentUpDownType = TargetUpDownType;
+            }
+        }
+        // UE_LOG(MainPlayerLog, Warning, TEXT("xxxxxx cur rotation pitch %f"), GetControlRotation().Pitch);
+    }
+#endif
 }
 
 void UDIY_MainPlayerCameraController::HandleMouseMoveForUpDownCam(const FVector2D &Value)
 {
-    	//UE_LOG(MainPlayerLog, Warning, TEXT("yYYYYY before pitch x %f, added pitch %f"), GetControlRotation().Pitch, Axis2DValue.Y * 5.0f);
+    // UE_LOG(MainPlayerLog, Warning, TEXT("yYYYYY before pitch x %f, added pitch %f"), GetControlRotation().Pitch, Axis2DValue.Y * 5.0f);
+#if 1
 
+    if (CurrentUpDownType == TargetUpDownType)
+    {
+        if (FMath::Abs(Value.Y) > UpDownCameraLerpTriggerThresHold)
+        {
 
+            if (Value.Y > 0)
+            {
+                if (TargetUpDownType >= 1)
+                {
+                    --TargetUpDownType;
+                }
+            }
+            else
+            {
+                if (TargetUpDownType < UpDownCameraType::Cam_Flying_Type)
+                {
+                    ++TargetUpDownType;
+                }
+            }
+        }
+    }
 
-	if(CurrentUpDownType==TargetUpDownType)
-	{
-		if (FMath::Abs(Value.Y) > UpDownCameraLerpTriggerThresHold)
-	{
+#endif
 
-		if (Value.Y > 0)
-		{
-			if (TargetUpDownType >= 1)
-			{
-				--TargetUpDownType;
-			}
+    AActor *owner_player = GetOwner();
 
+    APawn *cur_player_pawn = Cast<APawn>(owner_player);
 
+    if (nullptr != cur_player_pawn)
+    {
+        cur_player_pawn->AddControllerYawInput(Value.X);
+        // cur_player_pawn->AddControllerPitchInput(Value.Y);
+    }
 
-		}
-		else
-		{
-			if (TargetUpDownType < UpDownCameraType::Cam_Flying_Type)
-			{
-				++TargetUpDownType;
-			}
-
-
-		}
-	}
-	
-	}
-	
-
-	
-	//UE_LOG(MainPlayerLog, Warning, TEXT("xxxxxxxx after pitch %f"),GetControlRotation().Pitch);
+    // UE_LOG(MainPlayerLog, Warning, TEXT("xxxxxxxx after pitch %f"),GetControlRotation().Pitch);
 }
