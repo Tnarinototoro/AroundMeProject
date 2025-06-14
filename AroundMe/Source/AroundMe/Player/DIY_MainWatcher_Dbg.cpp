9dd5b6a314f9e5905c7d68709e30b8526bab50e4
Yorda <921864047@qq.com>
2025-06-14 19:11:11 +0900

    PG/GamePlay 将debug用cpp分割开来

---
+++ AroundMe/Source/AroundMe/Player/DIY_MainWatcher_Dbg.cpp
@@ -0,0 +1,1 @@
+#include "DIY_MainWatcher_Dbg.h"
---
+++ AroundMe/Source/AroundMe/Player/DIY_MainWatcher_Dbg.h
@@ -0,0 +1,20 @@
+// All rights reserved to ShadowCandle Studio
+
+#pragma once
+
+
+#include "CoreMinimal.h"
+#include "DIY_MainPlayer.h"
+#include "DIY_MainWatcher_Dbg.generated.h"
+
+UCLASS(
+    ClassGroup = (DIY_Debug),
+   
+    DisplayName = "DIY_MainWatcher_Dbg"
+
+)
+class AROUNDME_API ADIY_MainWatcher_Dbg : public ADIY_MainPlayer
+{
+    GENERATED_BODY()
+
+};
