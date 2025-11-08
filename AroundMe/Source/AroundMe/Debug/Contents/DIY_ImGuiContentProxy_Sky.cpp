b1607657991d93806a77ce90defce451e190404f
Yorda <921864047@qq.com>
2025-11-08 22:23:33 +0900

    PG/ Sky Debug追加
    主要是增加了 自動時間流逝相關的 debug menu
    并且 forcecontrol sky不開啓 不能控制天空
    并且 開啓自動流逝之後 無法 手動調節sky的時間

--- AroundMe/Source/AroundMe/Debug/Contents/DIY_ImGuiContentProxy_Sky.cpp
+++ AroundMe/Source/AroundMe/Debug/Contents/DIY_ImGuiContentProxy_Sky.cpp
@@ -12,41 +12,47 @@ FDIY_ImGuiContentProxy_Sky::FDIY_ImGuiContentProxy_Sky(FDIY_ImGuiProxy &InOwning
 void FDIY_ImGuiContentProxy_Sky::DrawWindow(float DeltaTime)
 {
     ImGui::Text("BasicControl");
+    ImGui::Checkbox("ForceControlSky", &bForceControlSky);
     ImGui::SliderFloat("Hour", &TimeOfDay, 0.f, 2400.f);
+    ImGui::Checkbox("EnableAnimatedTime", &bEnableAnimatedTime);
+    ImGui::SliderFloat("AnimatedTimeSpeed", &AnimatedTimeSpeed, 0.f, 100.f);
     ImGui::SliderFloat("OverallIntensity", &OverallIntensity, 0.f, 2.f);
-    ImGui::SliderFloat("Contrast", &Contrast, -0.2f,1.f);
-    ImGui::SliderFloat("Saturation", &Saturation, 0.f,1.5f);
+    ImGui::SliderFloat("Contrast", &Contrast, -0.2f, 1.f);
+    ImGui::SliderFloat("Saturation", &Saturation, 0.f, 1.5f);
 
-    UDIY_WeatherManager* WeatherManager = UDIY_WeatherManager::Get(GetWorld());
+    UDIY_WeatherManager *WeatherManager = UDIY_WeatherManager::Get(GetWorld());
 
-    if(nullptr == WeatherManager)
+    if (nullptr == WeatherManager)
     {
         return;
     }
 
-    ADIY_UDSParamsStore* Param_store=Cast<ADIY_UDSParamsStore>(WeatherManager->GetWorldParamsStoreInstance());
-   
-    if(nullptr == Param_store)
+    ADIY_UDSParamsStore *Param_store = Cast<ADIY_UDSParamsStore>(WeatherManager->GetWorldParamsStoreInstance());
+
+    if (nullptr == Param_store)
     {
         return;
     }
 
-    
-
-
-    
-
-    
 #if UE_BUILD_DEVELOPMENT || UE_BUILD_DEBUG
 
+    if (bForceControlSky)
+    {
+        Param_store->SetParam_EnableAnimatedTime(bEnableAnimatedTime);
 
+        if (!bEnableAnimatedTime)
+        {
+            Param_store->SetParam_TimeOfDay(TimeOfDay);
+        }
+        else
+        {
+            Param_store->SetParam_AnimatedTimeSpeed(AnimatedTimeSpeed);
+        }
+        Param_store->SetParam_OverallIntensity(OverallIntensity);
+        Param_store->SetParam_Contrast(Contrast);
+        Param_store->SetParam_Saturation(Saturation);
+    }
 
-    Param_store->SetParam_TimeOfDay(TimeOfDay);
-    Param_store->SetParam_OverallIntensity(OverallIntensity);
-    Param_store->SetParam_Contrast(Contrast);
-    Param_store->SetParam_Saturation(Saturation);
-
-    
 #endif
 }
 
