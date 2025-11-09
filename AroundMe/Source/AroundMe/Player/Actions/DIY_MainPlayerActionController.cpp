20d98a88d6f0d4439a2e89b9789450a422966b94
Yorda <921864047@qq.com>
2025-11-10 00:37:59 +0900

    PG/去掉莫名其妙的 debug line + format code

--- AroundMe/Source/AroundMe/Player/Actions/DIY_MainPlayerActionController.cpp
+++ AroundMe/Source/AroundMe/Player/Actions/DIY_MainPlayerActionController.cpp
@@ -3,7 +3,6 @@
 #include "AroundMe/GameUtilities/Logs/DIY_LogHelper.h"
 #include "GameFramework/Character.h"
 
-
 UDIY_MainPlayerActionController::UDIY_MainPlayerActionController()
 {
 
@@ -13,7 +12,7 @@ UDIY_MainPlayerActionController::UDIY_MainPlayerActionController()
 void UDIY_MainPlayerActionController::BeginPlay()
 {
     Super::BeginPlay();
-    
+
     InitWorldPosition = this->GetOwner()->GetActorLocation();
     InitRotator = this->GetOwner()->GetActorRotation();
 }
@@ -74,81 +73,62 @@ void UDIY_MainPlayerActionController::SetSimulatePhysics_Recursively(USceneCompo
 void UDIY_MainPlayerActionController::UpdateStateMachine(float inDelta)
 {
 
-
-    //keep translation
+    // keep translation
     {
         do
         {
-            //no need to do anything
-            if(nullptr==PickUpedActor)
+            // no need to do anything
+            if (nullptr == PickUpedActor)
             {
                 break;
             }
             ADIY_ItemBase *ItemBase = Cast<ADIY_ItemBase>(PickUpedActor);
 
-            //pickuped item must be a item base
-            if(nullptr==ItemBase)
+            // pickuped item must be a item base
+            if (nullptr == ItemBase)
             {
                 break;
             }
-            
+
             ACharacter *picker_player = Cast<ACharacter>(GetOwner());
 
-            //picker player must be a character
-            if(nullptr==picker_player)
+            // picker player must be a character
+            if (nullptr == picker_player)
             {
                 break;
             }
-            //EASY_LOG_MAINPLAYER("attached component: %s", *ItemBase->GetRootComponent()->GetAttachParent()->GetName())
+            // EASY_LOG_MAINPLAYER("attached component: %s", *ItemBase->GetRootComponent()->GetAttachParent()->GetName())
 
             USkeletalMeshComponent *picker_player_mesh = picker_player->GetMesh();
 
-            //player mesh must be valid
-            if(nullptr==picker_player_mesh)
+            // player mesh must be valid
+            if (nullptr == picker_player_mesh)
             {
                 break;
             }
 
-            USkeletalMeshComponent *attach_parent =Cast<USkeletalMeshComponent> (ItemBase->GetRootComponent()->GetAttachParent());
+            USkeletalMeshComponent *attach_parent = Cast<USkeletalMeshComponent>(ItemBase->GetRootComponent()->GetAttachParent());
 
-            //attach parent must be valid
-            if(nullptr==attach_parent)
+            // attach parent must be valid
+            if (nullptr == attach_parent)
             {
                 break;
             }
-            bool is_attached_to_player_main_mesh=(attach_parent==picker_player_mesh);
-
-
-
+            bool is_attached_to_player_main_mesh = (attach_parent == picker_player_mesh);
 
             const FTransform cur_item_pick_socket_trans = ItemBase->GetRootComponent()->GetSocketTransform("PickSocket");
             const FTransform cur_item_pick_socket_trans_relative = ItemBase->GetRootComponent()->GetSocketTransform("PickSocket", ERelativeTransformSpace::RTS_Actor);
-            
+
             FVector socket_loc = attach_parent->GetSocketLocation(ItemBase->GetRootComponent()->GetAttachSocketName());
-            
-            //EASY_LOG_MAINPLAYER("ItemBase->GetRootComponent()->GetAttachSocketName()=%s",*ItemBase->GetRootComponent()->GetAttachSocketName().ToString());
-          
-           
-           
+
+            // EASY_LOG_MAINPLAYER("ItemBase->GetRootComponent()->GetAttachSocketName()=%s",*ItemBase->GetRootComponent()->GetAttachSocketName().ToString());
+
             FVector relative_location = {socket_loc - cur_item_pick_socket_trans.GetLocation()};
             ItemBase->SetActorRelativeRotation(cur_item_pick_socket_trans_relative.GetRotation().Inverse());
             ItemBase->SetActorLocation(relative_location + ItemBase->GetActorLocation());
 
-       
-                
-            
-    
-    
-    
-    
-           
         } while (false);
-        
-    
     }
-
-
-
 }
 
 void UDIY_MainPlayerActionController::UpdateParams(float inDelta)
@@ -165,7 +145,7 @@ void UDIY_MainPlayerActionController::UpdateParams(float inDelta)
             EASY_LOG_MAINPLAYER("YYYYYYYYYYYY item launched!!!!");
             // BasicStaticMeshComponent->AddImpulse(PulseVec, NAME_None, true);
 
-           // BasicStaticMeshComponent->SetPhysicsLinearVelocity(PulseVec * mPickActionVelocityCoe);
+            // BasicStaticMeshComponent->SetPhysicsLinearVelocity(PulseVec * mPickActionVelocityCoe);
             PickUpedActor = nullptr;
         }
         if (nullptr != BasicStaticMeshComponent)
@@ -193,62 +173,47 @@ void UDIY_MainPlayerActionController::TickComponent(float DeltaTime, ELevelTick 
 
     UpdateParams(DeltaTime);
     UpdateStateMachine(DeltaTime);
-
-
-
-
 }
 
-void UDIY_MainPlayerActionController::PickUpDetectedItem(AActor *inActor, FName SocketName,  USkeletalMeshComponent* inAttachParentSKM)
+void UDIY_MainPlayerActionController::PickUpDetectedItem(AActor *inActor, FName SocketName, USkeletalMeshComponent *inAttachParentSKM)
 {
 
-
-    //already picked up something, you can not pick up again
-    if(nullptr!=PickUpedActor)
+    // already picked up something, you can not pick up again
+    if (nullptr != PickUpedActor)
     {
         return;
     }
 
-
-
-    //you can not pick up null actor
-    if(nullptr==inActor)
+    // you can not pick up null actor
+    if (nullptr == inActor)
     {
         return;
     }
 
-    //you can not pick up actor which is not a item actor
+    // you can not pick up actor which is not a item actor
     ADIY_ItemBase *ItemBase = Cast<ADIY_ItemBase>(inActor);
 
-    if(nullptr==ItemBase)
+    if (nullptr == ItemBase)
     {
         return;
-
     }
-
-
 
     UStaticMeshComponent *PickedUpActorBasicStaticMeshCompo = Cast<UStaticMeshComponent>(inActor->GetRootComponent());
 
-    //only actor which has static mesh component at root can be picked up
-    if(nullptr==PickedUpActorBasicStaticMeshCompo)
+    // only actor which has static mesh component at root can be picked up
+    if (nullptr == PickedUpActorBasicStaticMeshCompo)
     {
         return;
     }
 
-
     USkeletalMeshComponent *picker_mesh = inAttachParentSKM;
 
-    //if in attach parent is null, then use player mesh
-    if(nullptr==picker_mesh)
+    // if in attach parent is null, then use player mesh
+    if (nullptr == picker_mesh)
     {
-        picker_mesh=GetOwner()->FindComponentByClass<USkeletalMeshComponent>();
+        picker_mesh = GetOwner()->FindComponentByClass<USkeletalMeshComponent>();
     }
-    
 
-    
-
-  
     PickedUpActorBasicStaticMeshCompo->SetSimulatePhysics(false);
     // BasicStaticMeshComponent->SetCollisionEnabled(ECollisionEnabled::NoCollision);
     inActor->AttachToComponent(picker_mesh, FAttachmentTransformRules::KeepRelativeTransform, SocketName);
@@ -256,15 +221,12 @@ void UDIY_MainPlayerActionController::PickUpDetectedItem(AActor *inActor, FName 
     TargetPhysicsState = 1;
     PickUpedActor = inActor;
 
-
-    EASY_LOG_MAINPLAYER("Picked up the actor %s successfully",*inActor->GetName());
-
-
-
+    EASY_LOG_MAINPLAYER("Picked up the actor %s successfully", *inActor->GetName());
 }
 
 void UDIY_MainPlayerActionController::PlacePickedUpItem(bool inThrowFlag)
 {
+
     if (PickUpedActor)
     {
         ADIY_ItemBase *ItemBase = Cast<ADIY_ItemBase>(PickUpedActor);
@@ -281,10 +243,9 @@ void UDIY_MainPlayerActionController::PlacePickedUpItem(bool inThrowFlag)
 
             pulse_dir = pulse_dir.GetSafeNormal();
 
-
-            if(!inThrowFlag)
+            if (!inThrowFlag)
             {
-                pulse_dir=FVector::ZeroVector;
+                pulse_dir = FVector::ZeroVector;
             }
             ItemBase->DetachFromActor(FDetachmentTransformRules::KeepWorldTransform);
 
@@ -304,23 +265,18 @@ void UDIY_MainPlayerActionController::PlacePickedUpItem(bool inThrowFlag)
             float Thickness = 2.0f;
 
             // 绘制箭头
-            DrawDebugLine(GetWorld(), StartLocation, EndLocation, ArrowColor, true, 2.0f, 0, Thickness);
+            // DrawDebugLine(GetWorld(), StartLocation, EndLocation, ArrowColor, true, 2.0f, 0, Thickness);
 
-            
             HasImpulseTask = true;
 
-            
             PulseVec = pulse_dir;
             // ItemBase->GetItemDefualtConfig().ItemMass * mPickActionVelocityCoe;
-
-           
 
             PlacedActorBasicStaticMeshCompo->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
 
             TargetPhysicsState = 0;
 
-            EASY_LOG_MAINPLAYER("%s placed the actor %s successfully",*GetOwner()->GetName(),*PickUpedActor->GetName());
-           
+            EASY_LOG_MAINPLAYER("%s placed the actor %s successfully", *GetOwner()->GetName(), *PickUpedActor->GetName());
         }
     }
 }
