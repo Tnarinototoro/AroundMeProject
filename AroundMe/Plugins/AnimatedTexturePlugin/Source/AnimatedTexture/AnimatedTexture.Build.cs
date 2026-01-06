c8de6f1b77589dbc09c272e2dddbe94dc642f780
Yorda <921864047@qq.com>
2026-01-06 21:50:19 +0900

    PG/GIF 导入测试 OK

---
+++ AroundMe/Plugins/AnimatedTexturePlugin/Source/AnimatedTexture/AnimatedTexture.Build.cs
@@ -0,0 +1,31 @@
+// Copyright 2019 Neil Fang. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class AnimatedTexture : ModuleRules
+{
+    public AnimatedTexture(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+        DefaultBuildSettings = BuildSettingsVersion.V2;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+            }
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "RHI",
+                "RenderCore",
+            }
+            );
+        PublicDefinitions.Add("_CRT_HAS_CXX17=0");
+    }
+}
---
+++ AroundMe/Plugins/AnimatedTexturePlugin/Source/AnimatedTextureEditor/AnimatedTextureEditor.Build.cs
@@ -0,0 +1,32 @@
+// Copyright 2019 Neil Fang. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class AnimatedTextureEditor : ModuleRules
+{
+    public AnimatedTextureEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                "AnimatedTexture",
+            }
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "UnrealEd",
+                "RHI",
+                "RenderCore",
+            }
+            );
+        PublicDefinitions.Add("_CRT_HAS_CXX17=0");
+    }
+}
